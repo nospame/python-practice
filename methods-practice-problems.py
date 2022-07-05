909f1bbf3c30dc0edbc9f3e9d8a08a77ae979077
nospame <mapson.evan@gmail.com>
2022-07-05 14:43:43 -0700

    add methods practice problems, start on solutions

---
+++ methods-practice-problems.py
@@ -0,0 +1,24 @@
+# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+input_word = input("Enter a word: ")
+print(input_word.upper())
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+input_number = input("Enter a number: ")
+if int(input_number) > 100:
+    print("That's a big number")
+
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+# 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+# 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+# 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
