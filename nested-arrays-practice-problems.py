d0dba4334974aefb32ef169ebbb461158f37f463
nospame <mapson.evan@gmail.com>
2022-07-07 17:22:02 -0700

    add nested arrays practice solutions

--- nested-arrays-practice-problems.py
+++ nested-arrays-practice-problems.py
@@ -59,15 +59,61 @@ print(sum)
 
 #  6. Use a nested loop with two arrays of numbers to create a new array of the sums of each combination of numbers.
 #     For example, [1, 2] and [6, 7, 8] becomes [7, 8, 9, 8, 9, 10].
+list_a = [1, 2]
+list_b = [6, 7, 8]
+list_ab = []
+
+for num_a in list_a:
+    for num_b in list_b:
+        list_ab.append(num_a + num_b)
+
+print(list_ab)
+
 
 #  7. Use a nested loop with an array of numbers to compute an array with every combination of products from each number.
 #     For example, [2, 8, 3] becomes [4, 16, 6, 16, 64, 24, 6, 24, 9].
+list = [2, 8, 3]
+list_products = []
+
+for i in list:
+    for j in list:
+        list_products.append(i * j)
+
+print(list_products)
 
 #  8. Use a nested loop to find the largest sum of any two different numbers within an array.
 #     For example, [1, 8, 3, 10] becomes 18.
+list = [1, 8, 3, 10]
+large_sum = list[0]
+
+for i in range(len(list)):
+    for j in range(i + 1, len(list)):
+        sum = list[i] + list[j]
+        if sum > large_sum:
+            large_sum = sum
+
+print(large_sum)
 
 #  9. Use nested loops with an array of numbers to compute a new array containing the first two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
 #     For example, [2, 5, 3, 1, 0, 7, 11] becomes [3, 7].
+list = [2, 5, 3, 1, 0, 7, 11]
+target = 10
+output = False
+
+for i in range(len(list)):
+    for j in range(i + 1, len(list)):
+        if list[i] + list[j] == target:
+            output = [list[i], list[j]]
+
+print(output)
 
 # 10. Use a nested loop to convert an array of string arrays into a single string.
 #     For example, [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]] becomes "amanaplanacanalpanama".
+string_arrays = [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]]
+output_string = ""
+
+for sub_array in string_arrays:
+    for string in sub_array:
+        output_string += string
+
+print(output_string)
